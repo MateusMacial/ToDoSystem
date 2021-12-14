e1cf21d782b7dd7334a2b08428447ae422241253
mateusmacial <mateus_macial@hotmail.com>
2021-12-14 13:37:18 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/mateusmacial/todosystem/domain/ToDo.java
+++ src/main/java/com/mateusmacial/todosystem/domain/ToDo.java
@@ -3,10 +3,19 @@ package com.mateusmacial.todosystem.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ToDo implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; 
+	
 	private String descricao;
 	private Boolean realizada;
 	
