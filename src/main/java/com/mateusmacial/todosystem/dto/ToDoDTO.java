947223a22bf5f76e4dbc8a982796073f02576779
mateusmacial <mateus_macial@hotmail.com>
2021-12-15 14:23:15 -0300

    Validacoes personalizadas

--- src/main/java/com/mateusmacial/todosystem/dto/ToDoDTO.java
+++ src/main/java/com/mateusmacial/todosystem/dto/ToDoDTO.java
@@ -2,13 +2,21 @@ package com.mateusmacial.todosystem.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.mateusmacial.todosystem.domain.ToDo;
 
 public class ToDoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String descricao;
+	
 	private Boolean realizada;
 	
 	public ToDoDTO(){
--- src/main/java/com/mateusmacial/todosystem/resources/ToDoResource.java
+++ src/main/java/com/mateusmacial/todosystem/resources/ToDoResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.mateusmacial.todosystem.dto.ToDoDTO;
 import com.mateusmacial.todosystem.services.ToDoService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/todo")
 public class ToDoResource {
 	
@@ -46,6 +48,9 @@ public class ToDoResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ToDoDTO objDto, @PathVariable Integer id){
+		
+		//throw new RuntimeException("Erro");
+		
 		ToDo obj = toDoService.fromDTO(objDto);
 		obj.setId(id);
 		obj = toDoService.update(obj);
